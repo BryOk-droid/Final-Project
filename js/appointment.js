2c1515623bc3de2b69c5a7f8d0404d0d35e8555d
BryOk-droid <brianokoth534@gmail.com>
2025-02-05 17:47:06 +0300

    gave the appointment.html functionality

---
+++ js/appointment.js
@@ -0,0 +1,6 @@
+// Functionality for the appointment page
+document.addEventListener("DOMContentLoaded", () => {
+ console.log("Welcome to the MediCare Hub Appointment Page!");
+
+ const appointmentForm = document.getElementById("appointmentForm");
+ const confirmationMessage = document.getElementById("confirmationMessage");
