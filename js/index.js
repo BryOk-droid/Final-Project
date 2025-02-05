3996acf32d1b3084c29cf138bd7c460f4085aeb0
BryOk-droid <brianokoth534@gmail.com>
2025-02-05 17:55:58 +0300

    available services

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 // Functionality for the index.html
 document.addEventListener("DOMContentLoaded", function () {
  console.log("Welcome to the MediCare Hub Homepage!");
- 
+
+  // Log available services
+ console.log("Available Services:");
+ console.log("Service 1: General Checkup");
+ console.log("Service 2: Pediatrics");
+ console.log("Service 3: Cardiology");
+ console.log("Service 4: Dermatology");
