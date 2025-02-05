e5a3855e2a2138ad5081f8bce0ca00e1bea82e2a
BryOk-droid <brianokoth534@gmail.com>
2025-02-05 18:04:48 +0300

    Log activity when the button is clicked

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 // Functionality for the index.html
 document.addEventListener("DOMContentLoaded", function () {
- console.log("Welcome to the MediCare Hub Homepage!");
+  console.log("Welcome to the MediCare Hub Homepage!");
 
   // Log available services
- console.log("Available Services:");
- console.log("Service 1: General Checkup");
- console.log("Service 2: Pediatrics");
- console.log("Service 3: Cardiology");
- console.log("Service 4: Dermatology");
+  console.log("Available Services:");
+  console.log("Service 1: General Checkup");
+  console.log("Service 2: Pediatrics");
+  console.log("Service 3: Cardiology");
+  console.log("Service 4: Dermatology");
+
+  // Log activity when the "Book an Appointment" button is clicked
+  const bookAppointmentButton = document.querySelector(".btn");
+  bookAppointmentButton.addEventListener("click", function () {
+    console.log("User clicked on the 'Book an Appointment' button.");
+  });
+});
